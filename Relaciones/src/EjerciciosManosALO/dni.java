e8811e07819cd7dfb2c28d234accb57ab53f2bd6
Francisco Naranjo <Naranjofjx@gmail.com>
2023-05-15 23:14:37 -0400

    Primeras pruebas RELACIONES

---
+++ Relaciones/src/EjerciciosManosALO/dni.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjerciciosManosALO;
+
+/**
+ *
+ * @author JesusNjo
+ */
+public class dni {
+    
+    private char serie;
+    private int num;
+
+    public dni() {
+    }
+
+    public dni(char serie, int num) {
+        this.serie = serie;
+        this.num = num;
+    }
+
+    public char getSerie() {
+        return serie;
+    }
+
+    public void setSerie(char serie) {
+        this.serie = serie;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    
+    
+    
+    
+    
+}
